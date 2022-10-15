9e823db65daf55015a1b2481cb52e50a86354a40
Camila Heredia <cs.herediavalverde@gmail.com>
2022-10-14 21:42:46 -0300

    :sparkles: Feat: Create 10 variables to fulfill the first checkpoint using JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+const nombre = "Camila"; //String
+const apellido = "Heredia"; //String
+var edad = 24; //Number
+var celular = "1127002851"; //String
+var direccion = "Murguiondo 900"; //String
+var localidad = "CABA"; //String
+var estadoCivil = "soltera"; //String
+var tieneHijos = false; //Boolean
+var amigos = ["Sofía", "Lucas", "Jimena", "Santiago"]; //Array
+var madre = {
+    nombre: "Blanca",
+    edad: 54,
+    correo: "blancav@gmail.com",
+    telefono: 1185978625,
+    hobbies: ["cantar", "pescar", "correr"],
+} // Object
